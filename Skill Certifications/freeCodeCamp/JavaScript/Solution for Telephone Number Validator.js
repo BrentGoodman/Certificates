2c5124d884d9a2cc5d9e49774a7d86e2cec6bb1b
Brent Goodman <119774384+BrentGoodman@users.noreply.github.com>
2023-01-14 16:24:40 -0600

    Create Solution for Telephone Number Validator.js

---
+++ Skill Certifications/freeCodeCamp/JavaScript/Solution for Telephone Number Validator.js
@@ -0,0 +1,16 @@
+function telephoneCheck(str) {
+  const regex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/
+  return regex.test(str);
+  return true;
+}
+
+console.log(telephoneCheck("2 757 622-7382"));
+console.log(telephoneCheck("10 (757) 622-7382"));
+console.log(telephoneCheck("27576227382"));
+console.log(telephoneCheck("(275)76227382"));
+console.log(telephoneCheck("(555)5(55?)-5555"));
+console.log(telephoneCheck("1 555 555 5555"));
+console.log(telephoneCheck(1(555)555-5555));
+console.log(telephoneCheck((555)555-5555));
+console.log(telephoneCheck(555-555-5555));
+
